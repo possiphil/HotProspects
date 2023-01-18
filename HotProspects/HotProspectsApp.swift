a4b87a89516f7cbb5a9fc85430ff03957247420f
Philipp Sanktjohanser <venture@mein.gmx>
2023-01-18 05:41:58 +0100

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Philipp Sanktjohanser on 18.01.23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
